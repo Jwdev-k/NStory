29844f5df30305dd4d16473074d316058364de11
GomGom <ckaksj97@gmail.com>
2023-04-03 15:12:20 +0900

    url 오류 수정
    js 백업 및 닉네임길이 검증

--- src/main/java/nk/service/NStory/controller/LoginController.java
+++ src/main/java/nk/service/NStory/controller/LoginController.java
@@ -49,7 +49,7 @@ public class LoginController {
         if (userDetails != null) {
             return "redirect:" + request.getHeader("referer");
         }
-        if (email != null && password != null && name != null && !accountService.checkEmail(email)
+        if (email != null && password != null && name != null && name.length() >= 2 && !accountService.checkEmail(email)
                 && RecaptchaConfig.verify(token)) {
             accountService.register(new AccountDTO(0, email, passwordEncoder.encode(password), name, "", null
                     , "USER", CurrentTime.getTime(), null, 1, 0, 0, true));
