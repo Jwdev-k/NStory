ebe3a0e3fbb9f26d299fd150a97c54413b25f822
GomGom <ckaksj97@gmail.com>
2023-04-12 02:13:21 +0900

    UserDetails 관련 수정사항

--- src/main/java/nk/service/NStory/interceptor/GlobalInterceptor.java
+++ src/main/java/nk/service/NStory/interceptor/GlobalInterceptor.java
@@ -2,24 +2,16 @@ package nk.service.NStory.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nk.service.NStory.security.CustomUserDetails;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-@Component
+@Component @Slf4j
 public class GlobalInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (request.getUserPrincipal() != null && request.getAttribute("account") == null) { // 세큐리티 유저 객체 가져오기
-            SecurityContext context = SecurityContextHolder.getContext();
-            CustomUserDetails userDetails = (CustomUserDetails) context.getAuthentication().getPrincipal();
-            if (userDetails != null) {
-                request.setAttribute("account", userDetails);
-            }
-        }
+        log.info("접근 아이피주소: " + request.getRemoteHost());
         return true;
     }
 }
