7a3184fedf766baafcfeb62a0c96b59244f627dd
GomGom <ckaksj97@gmail.com>
2023-08-01 23:51:58 +0900

    @ServletComponentScan 삭제

--- src/main/java/nk/service/NStory/NStoryApplication.java
+++ src/main/java/nk/service/NStory/NStoryApplication.java
@@ -2,12 +2,10 @@ package nk.service.NStory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
-@ServletComponentScan
 @EnableScheduling @EnableMethodSecurity
 public class NStoryApplication {
 	public static void main(String[] args) {
